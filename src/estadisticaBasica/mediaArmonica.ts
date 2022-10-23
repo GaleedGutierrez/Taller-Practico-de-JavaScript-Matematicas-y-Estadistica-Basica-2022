88249d04c4535d188a1775294a62cd3cabb5e134
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-10-23 18:56:39 -0300

    :white_check_mark: Clase N°17: Reto: calcula otros tipos de promedio.

---
+++ src/estadisticaBasica/mediaArmonica.ts
@@ -0,0 +1,16 @@
+const LIST = [ 12, 17, 14, 8, 5 ];
+
+const arithmeticMean = (list: number[]) => {
+    const NUMBER_ELEMENTS = list.length;
+    let sumDivisor = 0;
+
+    for (const ITEMS of list) {
+        sumDivisor += 1 / ITEMS;
+    }
+
+    const RESULT = NUMBER_ELEMENTS / sumDivisor;
+
+    return Number(RESULT.toFixed(2));
+};
+
+console.log(arithmeticMean(LIST));
